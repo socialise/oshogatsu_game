50c0cfce00fb86481c1d16d9f34f061c1f1db10d
Soejima <hand36ideal@yahoo.co.jp>
2016-12-30 18:23:49 +0900

    session is ready

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
 ﻿//変数群
 const NANASHI = 'guest';    //ゲストのクッキー名
 const MAX = 5;              //ランキング表示数
+var cookie;                 //クッキー
 
 //読み込み
 var http = require('http');
 var json = require('./user.json'); //jsonファイルの取得
 var qs = require('querystring');
 var url = require('url');
+var fs = require('fs');
+
 
 http.createServer(function (request, res) {
     var error_message = null;
 
     if (request.method == 'GET') { 
+        
+        //var q = url.parse(request.url, true);
+        var query = url.parse(request.url).query;
+        var name = qs.parse(query).name
+        if (name) {
+            res.setHeader('Set-Cookie', ['name=' + encodeURIComponent(name)]);
+        }
+        //console.log(query);
+        //console.log(qs.parse(query).name);
+        cookie = request.headers.cookie;
+        console.log("cookie", cookie);
 
-        if (request.url == '/') { 
-            var cookie = request.headers.cookie;
-            if (!cookie) {
-                //登録画面に移行する
-                console.log('<<guest enter>>');
-
-                var fs = require('fs');
-                fs.readFile('./register_form.html', 'utf-8', function (err, data) {
-                    if (err) {
-                        res.writeHead(404, { 'Content-Type': 'text/plain' });
-                        res.write('not found!');
-                        return res.end();
-                    }
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(data);
-                    res.end();                          //responseを閉じる - guest_page
-                });
-                
-            } else {
+        if (!cookie) {
+            console.log("<<guest enter>>");
+            makingGuestPage(res);
+            res.end();
+        } else if (request.url == '/' || cookie) { 
+            
+            
                 console.log("---メインページを表示しました。---")
                 makingMainPage(res);
-                res.end();                              //responseを閉じる - main_page
-            }
-            
+                res.end();                                  //responseを閉じる - main_page
+                //fs.writeFile('user.json', JSON.stringify(json, null, '    ')); //JSONを上書き
+            //}
         } else if (request.url == '/member') {
             res.writeHead(200, { 'Content-Type': 'application/json' });
 
             res.write("{");
-            //jsonの走査
+            //jsonの作成（名前のみ）
             for (var i = 0; i < json.length; i++) {
                 var obj = json[i];
-                if (obj != null) {
+                if (obj) {
                     res.write(obj.name);
                 } else {
                     continue;
@@ -68,7 +70,7 @@ http.createServer(function (request, res) {
             var name = post.name;
             var error = false;
             
-            console.log("--- 令コード：" + op);
+            console.log("--- 命令コード：" + op);
             console.log("POST-DATA", post);
             switch (op) {
                 case '1': //ユーザ登録
@@ -107,7 +109,7 @@ http.createServer(function (request, res) {
 
             }
             if (!error) {
-                var fs = require('fs');
+                
                 fs.writeFile('user.json', JSON.stringify(json, null, '    ')); //JSONを上書き
                 res.write('successed processing');
                 res.end();
@@ -133,10 +135,7 @@ var sort_by = function (field, reverse) {
     }
 }
 
-function makingMainPage(res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-
-    var text =
+var header =
         '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' +
         '<html lang="ja">' +
         '<head>' +
@@ -154,7 +153,10 @@ function makingMainPage(res) {
         '</head>' +
         '<body>';
 
-    res.write(text);
+function makingMainPage(res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.write(header);
 
     res.charset = 'utf-8'
     
@@ -162,6 +164,8 @@ function makingMainPage(res) {
 
     //参加者の得点リスト 作成
     res.write('<ol>');
+    var name = decodeURIComponent(cookie.substring(5))
+    console.log('表示テスト', name);
 
     json.sort(sort_by('point', true)); //降順ソート
     for (var i = 0; i < MAX && i < json.length; i++) { //変数の値まで表示
@@ -176,4 +180,30 @@ function makingMainPage(res) {
 
     res.write('</body>');
     res.write('</html>');
+}
+
+function makingGuestPage(res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(header);
+    res.charset = 'utf-8'
+    res.write('<form action="http://localhost:8080" method="GET">');
+    res.write('<select name="name">');
+
+    for (var i = 0; i < json.length; i++) {
+        var obj = json[i];
+        if (obj != null)
+            res.write('<option value="' + obj.name + '">' + obj.name + '</option>');
+
+        //res.write('<li id="' + i + '">' + obj.name + ':' + obj.point + 'pt</li>');
+    }
+
+    //'<option value="' + name + '">' + name + '</option>'
+
+    res.write('</select>');
+    res.write('<input type="submit" value="送信">');
+    res.write('</form>');
+    res.write('ボタンを押したら、更新ボタン(F5)を押してください。')
+    res.write('</body>');
+    res.write('</html>');
+    
 }
