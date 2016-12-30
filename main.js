6531b2fba8053ef81c55c918ac5e9728f09e1da0
Soejima <hand36ideal@yahoo.co.jp>
2016-12-30 12:01:45 +0900

    fix bug that null occurs in JSON when removing elements

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 ﻿var http = require('http');
 var json = require('./user.json'); //jsonファイルの取得
 var qs = require('querystring');
+var url = require('url');
 
 //jsonのソート関数
 var sort_by = function (field, reverse) {
@@ -17,28 +18,31 @@ var sort_by = function (field, reverse) {
 
 http.createServer(function (request, res) {
     var error_message = null;
-    if (request.method != 'POST') {
-        res.charset = 'utf-8'
-        writeHeader(res); //ヘッダー
 
-        res.write('<h1>参加者得点一覧</h1>');
+    if (request.method == 'GET') {
 
-        //参加者の得点リスト 作成
-        res.write('<ol>');
+        if (request.url == '/') {
+            console.log("write")
+            makingMainPage(res);
 
-        json.sort(sort_by('point', true)); //降順ソート
-        for (var i = 0; i < json.length; i++) {
-            var obj = json[i];
-            if (obj != null)
-                res.write('<li id="' + i + '">' + obj.name + ':' + obj.point + 'pt</li>');
+        } else if (request.url == '/member') {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+
+            res.write("{");
+            //jsonの走査
+            for (var i = 0; i < json.length; i++) {
+                var obj = json[i];
+                if (obj != null) {
+                    res.write(obj.name);
+                } else {
+                    //res.write('null');
+                    continue;
+                }
+                //末尾でなければカンマをつける
+                if (i < json.length - 1) res.write(',');
+            }
+            res.write("}");
         }
-        res.write('</ol>');
-
-        //特定ユーザの得点表示機能
-        res.write('あなたのポイントは:');
-
-
-        writeFooter(res); //フッター
     } else {
         var body = '';
 
@@ -50,47 +54,48 @@ http.createServer(function (request, res) {
             var op = post.op;
             var name = post.name;
             var error = false;
-            console.log("ポストデータ：", post);
-
+            console.log("POST-DATA", post);
+            console.log("---命令コード：" + op);
             switch (op) {
                 case '1': //ユーザ登録
                     var obj = {
                         "name": name,
                         "point": 0
                     }
-                    json[json.length] = obj;
+                    json.push(obj);
                     console.log("ユーザ作成", obj);
                     break;
                 case '2': //得点操作
-                case '3': //ユーザ削除
 
                     var index = -1;
                     for (var i = 0; i < json.length; i++) {
                         var obj = json[i];
-                        if (obj != null || obj.name == name) index = i;
+                        if (obj != null && obj.name == name) index = i;
                     }
-
                     if (index == -1) {
                         error = true;
                         error_message = "該当データはありませんでした。"
                         break;
                     }
 
-                    if (op == '3') {
-                        //削除処理
-                        console.log(name + "さんを削除します");
-                        delete json[index];
-                        break;
-
-                    } else {
-                        //得点加算
-                        var point = Number(post.point);//加算分
+                    if (op == '2') {
+                        console.log(name + "さんのポイントを" + point + "加算します。")
+                        var point = Number(post.point); //加算分
                         json[index].point += point;
                         break;
                     }
+                case '3': //ユーザ削除処理
+                    console.log(name + "さんを削除します");
+
+                    json = json.filter(function (v) {
+                        return v.name != name
+                    });
+
+                    //delete json[index];
+                    break;
+
             }
             if (!error) {
-                console.log("json変更");
                 var fs = require('fs');
                 fs.writeFile('user.json', JSON.stringify(json, null, '    '));
                 res.write('successed processing');
@@ -105,17 +110,15 @@ http.createServer(function (request, res) {
 
 console.log('Server running on 8080');
 
-
-function writeHeader(res) {
-
+function makingMainPage(res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    var text = 
+    var text =
         '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' +
         '<html lang="ja">' +
         '<head>' +
         '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">' +
-        
+
         //CSS記述
         '<style type="text/css">' +
             'h1{' +
@@ -127,11 +130,27 @@ function writeHeader(res) {
         '<title>お正月ゲーム大会</title>' +
         '</head>' +
         '<body>';
-    
-    res.write(text);
-}
 
-function writeFooter(res) {
+    res.write(text);
+
+    res.charset = 'utf-8'
+    
+    res.write('<h1>参加者得点一覧</h1>');
+
+    //参加者の得点リスト 作成
+    res.write('<ol>');
+
+    json.sort(sort_by('point', true)); //降順ソート
+    for (var i = 0; i < json.length; i++) {
+        var obj = json[i];
+        if (obj != null)
+            res.write('<li id="' + i + '">' + obj.name + ':' + obj.point + 'pt</li>');
+    }
+    res.write('</ol>');
+
+    //特定ユーザの得点表示機能
+    res.write('あなたのポイントは:');
+
     res.write('</body>');
     res.write('</html>');
 }
